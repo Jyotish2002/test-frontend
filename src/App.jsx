2a74a58b5d627340ed8d05f74f33132869c7cebd
Jyotish Yadav <136741028+Jyotish2002@users.noreply.github.com>
2024-12-02 03:14:08 +0530

    Route update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { HashRouter as Router, Route,  } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/home/Home";
 import Header from "./components/header/Header";
